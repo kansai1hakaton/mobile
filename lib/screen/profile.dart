7a8ac7879fe9e8dd655b331a6cb92b8905780f49
yudaitakeda <yudai.1009.takeda@icloud.com>
2024-03-09 10:08:38 +0900

    feat:flutter_hooksパッケージをインポート

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
