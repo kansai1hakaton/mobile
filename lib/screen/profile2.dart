859e7ea2bb60faa5343339dd72aeb647384c752b
yudaitakeda <yudai.1009.takeda@icloud.com>
2024-03-09 15:32:16 +0900

    feat:編集画面の修正

--- lib/screen/profile2.dart
+++ lib/screen/profile2.dart
@@ -8,21 +8,91 @@ class Profile2Screen extends HookWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Profile'),
+          title: Text('Profile '),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+        body: Column(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: ElevatedButton(
+                    child: Text("編集"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
             Padding(
-              padding: EdgeInsets.only(right: 20),
-              child: ElevatedButton(
-                child: Text("編集"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProfileScreen()),
-                  );
-                },
+              padding: EdgeInsets.only(top: 50),
+              child: Center(
+                child: Icon(Icons.account_circle, size: 96), // アイコンを配置
+              ),
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(right: 150, left: 150, bottom: 20), //左、右、上、下
+              child: TextField(
+                textAlign: TextAlign.center, // テキストを中央揃えにする
+                style: TextStyle(fontSize: 24),
+                decoration: InputDecoration(
+                  hintText: "name",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 10), //horizonta:左右、vertical上下
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: "nickname",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 10), //horizonta:左右、vertical上下
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.cake),
+                  hintText: "birthday",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 10), //horizonta:左右、vertical上下
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.edit),
+                  hintText: "comment",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 10), //horizonta:左右、vertical上下
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.directions_walk),
+                  hintText: "have visited",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 50, vertical: 20), //horizonta:左右、vertical上下
+              child: Text(
+                "書いた記事",
+                style: TextStyle(fontSize: 25),
               ),
             ),
           ],
