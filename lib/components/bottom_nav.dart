a41031a32cccd29efcf0ecda32c107d21b17bc34
hwacho9 <csh77776@gmail.com>
2024-03-10 08:45:31 +0900

    feat:add bottom nav

---
+++ lib/components/bottom_nav.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  const BottomNav({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          label: 'Business',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+        ),
+      ],
+    );
+  }
+}
