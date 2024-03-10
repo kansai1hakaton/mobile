4e9b7dd44bdc4cd443af4e5ff968e587ae010897
hwacho9 <csh77776@gmail.com>
2024-03-10 13:09:11 +0900

    feat:add city screen

--- lib/screen/city.dart
+++ lib/screen/city.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
 
-class CityScreen extends StatelessWidget {
+class CityScreen extends StatefulWidget {
   const CityScreen({super.key});
+
+  @override
+  State<CityScreen> createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  final _key = GlobalKey<FormState>();
+  final TextEditingController _cityController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Local'),
+        title: Text('City'),
       ),
       body: Center(
-        child: Text('Local'),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [cityInput(), submitButton()],
+          ),
+        ),
       ),
+    );
+  }
+
+  TextFormField cityInput() {
+    return TextFormField(
+      controller: _cityController,
+      autofocus: true,
+      decoration: const InputDecoration(
+        labelText: 'City',
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your city';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  ElevatedButton submitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_key.currentState!.validate()) {
+          Navigator.pushNamed(context, '/home');
+        }
+      },
+      child: const Text('Submit'),
     );
   }
 }
