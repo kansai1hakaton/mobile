1caaa1c629f667b8c8f1829218bd75751c431d7d
hwacho9 <csh77776@gmail.com>
2024-03-11 11:49:04 +0900

    delete:auth.dart

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -62,85 +62,84 @@ class _ProfileScreenState extends State<ProfileScreen> {
           builder: (context, snapshot) {
             return Scaffold(
               body: Center(
-                child: Column(children: [
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 15),
-                    child: Center(
-                        child: Image.network(
-                            'https://366e203a.rocketcdn.me/wp-content/uploads/2021/03/wealthy-photographer.jpg') // アイコンを配置
-                        ),
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                  child: Column(children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 15),
+                  child: Center(
+                      child: Image.network(
+                          'https://366e203a.rocketcdn.me/wp-content/uploads/2021/03/wealthy-photographer.jpg') // アイコンを配置
                       ),
-                      Icon(Icons.email), // メールアイコン
-                      SizedBox(width: 10), // アイコンとテキストの間隔
-                      Text(
-                        (snapshot.data as QuerySnapshot).docs[0]['email'],
-                        style: GoogleFonts.ubuntu(
-                            fontSize: 30, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                      ),
-                      Icon(Icons.person), // 人物アイコン
-                      SizedBox(width: 10), // アイコンとテキストの間隔
-                      Text(
-                        (snapshot.data as QuerySnapshot).docs[0]['name'],
-                        style: GoogleFonts.ubuntu(
-                            fontSize: 30, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                      ),
-                      Icon(Icons.person_outline), // ニックネームアイコン
-                      SizedBox(width: 10), // アイコンとテキストの間隔
-                      Text(
-                        (snapshot.data as QuerySnapshot).docs[0]['nickname'],
-                        style: GoogleFonts.ubuntu(
-                            fontSize: 30, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.end,
-                  //   children: [
-                  //     ElevatedButton(
-                  //       onPressed: () {
-                  //         // ボタンが押された時の処理
-                  //       },
-                  //       style: ElevatedButton.styleFrom(
-                  //         shape: RoundedRectangleBorder(
-                  //           borderRadius: BorderRadius.circular(10), // 角の丸みを調整
-                  //         ),
-                  //       ),
-                  //       child: Text('DM'),
-                  //     ),
-                  //     ElevatedButton(
-                  //       onPressed: () {
-                  //         // ボタンが押された時の処理
-                  //       },
-                  //       style: ElevatedButton.styleFrom(
-                  //         shape: RoundedRectangleBorder(
-                  //           borderRadius: BorderRadius.circular(10), // 角の丸みを調整
-                  //         ),
-                  //       ),
-                  //       child: Text('予約'),
-                  //     )
-                  //   ],
-                  // ),
-                ]),
-              ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    ),
+                    Icon(Icons.email), // メールアイコン
+                    SizedBox(width: 10), // アイコンとテキストの間隔
+                    Text(
+                      (snapshot.data as QuerySnapshot).docs[0]['email'],
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 30, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    ),
+                    Icon(Icons.person), // 人物アイコン
+                    SizedBox(width: 10), // アイコンとテキストの間隔
+                    Text(
+                      (snapshot.data as QuerySnapshot).docs[0]['name'],
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 30, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    ),
+                    Icon(Icons.person_outline), // ニックネームアイコン
+                    SizedBox(width: 10), // アイコンとテキストの間隔
+                    Text(
+                      (snapshot.data as QuerySnapshot).docs[0]['nickname'],
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 30, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.end,
+                //   children: [
+                //     ElevatedButton(
+                //       onPressed: () {
+                //         // ボタンが押された時の処理
+                //       },
+                //       style: ElevatedButton.styleFrom(
+                //         shape: RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(10), // 角の丸みを調整
+                //         ),
+                //       ),
+                //       child: Text('DM'),
+                //     ),
+                //     ElevatedButton(
+                //       onPressed: () {
+                //         // ボタンが押された時の処理
+                //       },
+                //       style: ElevatedButton.styleFrom(
+                //         shape: RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(10), // 角の丸みを調整
+                //         ),
+                //       ),
+                //       child: Text('予約'),
+                //     )
+                //   ],
+                // ),
+              ])),
             );
           }),
     );
